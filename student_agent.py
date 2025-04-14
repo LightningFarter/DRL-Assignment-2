de2231b5e0e1b889cf6bf1387e246a83e1a4771c
LightningFarter <jason5738867@gmail.com>
2025-04-14 17:21:49 +0800

    debug

--- student_agent.py
+++ student_agent.py
@@ -315,7 +315,8 @@ class CPPModel:
                 name = f.read(name_len).decode('utf-8')
                 
                 # Parse pattern from name (e.g., "4-tuple pattern 0123")
-                pattern = [int(c, 16) for c in name.split()[-1] if c.isalnum()]
+                print(name)
+                pattern = [int(c, 16) for c in name.split()[-1]]
                 
                 # Create pattern and load weights
                 p = Pattern(pattern)
